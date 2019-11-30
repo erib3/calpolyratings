14bf2a76b5798508573fd22bdecf499b57314c22
eribe403 <eribe403@redfern.dreamhost.com>
2019-11-29 21:20:28 -0800

    new url to serve static files in production

--- cpratings/urls.py
+++ cpratings/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path, path, include
 from cpratings.use_secrets import *
 
 
 urlpatterns = [
     path(get_secret("ADMIN_URL"), admin.site.urls),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT,}),
     path('', include('home.urls'))
 ]
